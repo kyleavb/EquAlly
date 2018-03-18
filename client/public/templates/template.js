16929e7dea51995397819a9933ae64a75213f36e
Emily Marie Garrett <e.marie.garrett@gmail.com>
2018-03-18 13:01:15 -0700

    added all-blog-page and single-blog-page styling

--- client/public/templates/template.js
+++ client/public/templates/template.js
@@ -5,6 +5,11 @@
 	$(".dropdown-button").dropdown({hover: false});
 	//carousel initialization
 	$('.carousel.carousel-slider').carousel({fullWidth: true, indicators: true});
+	function autoplay() {
+    	$('.carousel').carousel('next');
+    	setTimeout(autoplay, 4500);
+	}
+	autoplay();
 	//tabs init
 	$('ul.tabs').tabs();
 	//dropdown menu init
