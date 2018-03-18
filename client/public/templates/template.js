e184f2e283a264018dbe7146f33527aa9b3b50cc
Emily Marie Garrett <e.marie.garrett@gmail.com>
2018-03-17 22:32:46 -0700

    styled the full front page template

---
+++ client/public/templates/template.js
@@ -0,0 +1,10 @@
+// $(document).ready(function) {
+	//mobile hamburger menu activator for nav
+	$('.button-collapse').sideNav();
+	//dropdown button on nav
+	$(".dropdown-button").dropdown({hover: false});
+	//carousel initialization
+	$('.carousel.carousel-slider').carousel({fullWidth: true, indicators: true});
+	//tabs init
+	$('ul.tabs').tabs();
+// }
