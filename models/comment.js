a6873fdde58910de6e08c28ca733223de64d6190
Daniel Heyward <danheyward@gmail.com>
2018-03-19 13:16:34 -0700

    added models

---
+++ models/comment.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var commentSchema = new mongoose.Schema({
+  _id: {
+    type: ObjectId
+  },
+  userId: {
+    type: ObjectId,
+    ref: 'User'
+  },
+  postId: {
+    type: ObjectId,
+    ref: 'Post'
+  },
+  comment: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.Now
+  }
+})
+
+
+var Comment = mongoose.model('Comment', commentSchema)
+
+module.exports = Comment;
---
+++ models/post.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var postSchema = new mongoose.Schema({
+  _id: {
+    type: ObjectId
+  },
+  userId: {
+    type: ObjectId,
+    ref: 'User'
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.Now
+  },
+  comments: [{
+    type: ObjectId,
+    ref: 'Comment'
+  }]
+})
+
+
+var Post = mongoose.model('Post', postSchema)
+
+module.exports = Post;
