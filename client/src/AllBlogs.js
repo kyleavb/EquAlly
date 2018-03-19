21149c2254b79c2f392f431e21a324d97ba0000c
Emily Marie Garrett <e.marie.garrett@gmail.com>
2018-03-19 16:07:16 -0700

    created a new component for all blog, still need to style

---
+++ client/src/AllBlogs.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { Row, Col, Button } from 'react-materialize';
+
+class AllBlogs extends Component {
+	
+	render() {
+		return(
+
+
+		)
+	}
+
+}
+
+export default AllBlogs;
