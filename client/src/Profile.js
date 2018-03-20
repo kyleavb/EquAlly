f7539891652b52647a668afdc447f70585c717f7
Emily Marie Garrett <e.marie.garrett@gmail.com>
2018-03-20 10:57:20 -0700

    profile fix

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -2,22 +2,6 @@ import React, { Component } from 'react';
 import { Row, Col, Input } from 'react-materialize';
 import JournalEntryTeaser from './JournalEntryTeaser';
 
-
-var textContainer, textareaSize, input;
-	var autoSize = function () {
-	  // also can use textContent or innerText
-	  textareaSize.innerHTML = input.value + '\n';
-	};
-
-	document.addEventListener('DOMContentLoaded', function() {
-	  textContainer = document.querySelector('.textarea-container');
-	  textareaSize = textContainer.querySelector('.textarea-size');
-	  input = textContainer.querySelector('textarea');
-
-	  autoSize();
-	  input.addEventListener('input', autoSize);
-	});
-
 class Profile extends Component {
 
 	render() {
