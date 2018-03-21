d1a4ee02a60cd9ffdc18b3c9455ec10e10e2ccef
Emily Marie Garrett <e.marie.garrett@gmail.com>
2018-03-21 14:46:59 -0700

    made carousel autoplay

--- client/src/Carousel.js
+++ client/src/Carousel.js
@@ -4,8 +4,18 @@ import CarouselImage1 from './img/couple-walking-sm.jpg';
 import CarouselImage2 from './img/seattle-trans-pride-march-sm.jpg';
 import CarouselImage3 from './img/hands-sm.jpg';
 import CarouselImage4 from './img/heart-hands-sm.jpg';
+declare var $: any;
 
-export default () => (
+
+export default () => {   
+
+	autoplay()   
+	function autoplay() {
+    	$('.carousel').carousel('next');
+    	setTimeout(autoplay, 4500);
+	}
+
+	return (
 		<Carousel
 			fixedItem={
 				<div>
@@ -15,12 +25,18 @@ export default () => (
 				<a className='btn waves-effect pink lighten-2 white-text' href='/chat'>Chat now!</a>
 				</div>
 			}
+
 			options={{fullWidth: true, indicators: true}}
+
 			images={[
 				CarouselImage1,
 				CarouselImage2,
 				CarouselImage3,
 				CarouselImage4
 				]}
+
 		/>
+
 	)
+
+}
